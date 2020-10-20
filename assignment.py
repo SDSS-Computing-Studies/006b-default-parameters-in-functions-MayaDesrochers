9dd974e10cbe188432f8d09d75d070967d2a99c5
MayaDesrochers <mayad459@deltalearns.ca>
2020-10-20 11:03:58 -0700

    update

--- assignment.py
+++ assignment.py
@@ -2,18 +2,38 @@
 
 import math
 
-def tempConversion():
+def tempConversion(degrees, unit="a"):
+    degrees=float(degrees) 
+    if unit=="C":
+        x=(degrees*(9/5) )+ 32
+    elif unit=="F":
+        x=(degrees-32)/1.8
+    return round(x,1)
+
+def factorPair(a,b):
+    x=a/b
+    factor_list=[]
+    factor_list.append(b)
+    factor_list.append(x)
+    
+    return factor_list
 
 
-def factorPair():
+
+def cosineLaw(a,b,c, by="oppositeSide"):
+    import math 
+        "oppositeSide"==True:
+        side1=a
+        side2=b
+        angle=c
 
 
+    
+  
+    
+#def toRadians():
 
-def cosineLaw():
+#def solution():
 
-def toRadians():
-
-def solution():
-
-def quadratic():
+#def quadratic():
 
--- task2.py
+++ task2.py
@@ -1,2 +1,6 @@
 #!python3
 
+import assignment
+
+print( assignment.factorPair(10,2) ) 
+ 
