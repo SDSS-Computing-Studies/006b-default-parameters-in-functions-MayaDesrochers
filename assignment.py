2608e32c53a92588d48e2e4a965d4567f0f7ee27
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-11-15 17:55:25 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -18,22 +18,33 @@ def factorPair(a,b):
     
     return factor_list
 
+def cosineLaw(side1,side2,angle, side3="oppositeSide"):
+    a=side1
+    b=side2
+ 
+    side3= math.sqrt((a**2)+(b**2)-((2*a*b)*math.cos(radians)))
 
+    return side3
 
-def cosineLaw(a,b,c, by="oppositeSide"):
-    import math 
-        "oppositeSide"==True:
-        side1=a
-        side2=b
-        angle=c
-
-
-    
+def toRadians(angle):
   
-    
-#def toRadians():
+    angle=float(angle)
+    radians=(angle*(math.pi))/180
+    return radians
 
-#def solution():
+def solution(answerList):
+    answer=answerList.pop(1)
+    return answer
 
-#def quadratic():
+def quadratic(side1, side2, side3):
+    answerList=[]
+    a=side1
+    b=side2
+    c=side3
+    answer1=((-b)+(math.sqrt((b**2)-(4*a*c))))/(2*a)
+    answer2=((-b)-(math.sqrt((b**2)-(4*a*c))))/(2*a)
+    answerList.append(answer1)
+    answerList.append(answer2)
+    answerList.sort()
+    return answerList
 
