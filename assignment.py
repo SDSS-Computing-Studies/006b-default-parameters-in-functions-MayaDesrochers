358cd5b02e51a6c48d9bb559535f9ece65f0e10b
MayaDesrochers <71406875+MayaDesrochers@users.noreply.github.com>
2020-11-16 18:45:39 -0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -18,33 +18,53 @@ def factorPair(a,b):
     
     return factor_list
 
-def cosineLaw(side1,side2,angle, oppositeSide="True"):
-    a=side1
-    b=side2
-    radians=toRadians(angle)
-    side3= math.sqrt((a**2)+(b**2)-((2*a*b)*math.cos(radians)))
+def cosineLaw(a,b,angle, oppositeSide=True):
+    a=float(a)
+    b=float(b)
+    angle=toRadians(angle)
+     
+    if oppositeSide==True:
 
-    return side3
+        side_c= (a**2 + b**2)-(2*a*b*math.cos(angle))
+        side_c=math.sqrt(side_c)
+        return side_c 
+
+    elif oppositeSide==False:
+        if a>b:
+            a1 = 1
+            b1 = (-2*b*math.cos(angle))
+            c1 = (b**2 - a**2)
+
+            x1_and_x2 = quadratic(a1,b1,c1)
+            side_c = solution(x1_and_x2)
+            return side_c 
+    
+        if b>a:
+            a1 = 1
+            b1 = (-2*a*math.cos(angle))
+            c1 = a**2 - b**2 
+
+            x1_and_x2 = quadratic(a1,b1,c1)
+            side_c = solution(x1_and_x2)
+            return side_c 
 
 def toRadians(angle):
-  
     angle=float(angle)
-    radians=(angle*(math.pi))/180
+    radians=(angle*math.pi)/180
     return radians
 
 def solution(answerList):
     answer=answerList.pop(1)
     return answer
 
-def quadratic(side1, side2, side3):
-    answerList=[]
-    a=side1
-    b=side2
-    c=side3
-    answer1=((-b)+(math.sqrt((b**2)-(4*a*c))))/(2*a)
-    answer2=((-b)-(math.sqrt((b**2)-(4*a*c))))/(2*a)
-    answerList.append(answer1)
-    answerList.append(answer2)
+def quadratic(a,b,c):
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    discriminant = math.sqrt((b**2)-(4*a*c))
+
+    answer1 = (-b + discriminant)/(2*a)
+    answer2 = (-b - discriminant)/(2*a)
+    answerList = [answer1, answer2]
     answerList.sort()
     return answerList
-
